090c8f0c39da1272d2354b5137db3a2c84c8af70
Nattha <natthakit.khamso@gmail.com>
2023-11-01 11:35:36 +0100

    check for app env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
+const IN_DEVELOPMENT = process.env.NODE_ENV !== 'production'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: IN_DEVELOPMENT,
+
+    timeline: {
+      enabled: true,
+    },
+  },
 
   modules: [
     'nuxt-graphql-client'
