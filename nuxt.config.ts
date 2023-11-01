65d7f72f44b56ab9379208c01e72b94bf1e1248f
Nattha <natthakit.khamso@gmail.com>
2023-11-01 11:00:15 +0100

    add breaky component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     'nuxt-graphql-client'
+  ],
+
+  components: [
+    {
+      path: '~/components/base',
+      global: true,
+    },
   ]
 })
